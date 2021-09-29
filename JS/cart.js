b2e150877d936292fc44aec21546b60b159a5425
matan-ysayas <matan.ysayas@gmail.com>
2021-09-29 10:45:44 +0300

    cart fixed

--- JS/cart.js
+++ JS/cart.js
@@ -13,16 +13,19 @@ tableContainer.innerHTML=`
         <td class="productsNameTd">${Products[9].name}</td>
         <td class="productsPriceTd">${Products[9].price}$</td>
         <td class="productsImgTd"><img src="${Products[9].images[0]}"class="productsImg"></td>
+        <td><button class="removeFromCartBut">remove</button></td>
         </tr>
         <tr class="tableRow">
         <td class="productsNameTd">${Products[18].name}</td>
         <td class="productsPriceTd">${Products[18].price}$</td>
         <td class="productsImgTd"><img src="${Products[18].images[0]}"class="productsImg"></td>
+        <td><button class="removeFromCartBut">remove</button></td>
         </tr>
         <tr class="tableRow">
         <td class="productsNameTd">${Products[32].name}</td>
         <td class="productsPriceTd">${Products[32].price}$</td>
         <td class="productsImgTd"><img src="${Products[32].images[0]}" class="productsImg"></td>
+        <td><button class="removeFromCartBut">remove</button></td>
         </tr>
 </table>
 <section id="totalPriceContainer" >
@@ -35,6 +38,26 @@ tableContainer.innerHTML=`
 let productsPriceTd=document.getElementsByClassName("productsPriceTd");
 let totalPrice=0;
 let totalPriceContainer=document.getElementById("totalPriceContainer");
+let removeFromCartBut=document.getElementsByClassName("removeFromCartBut");
+let tableRow=document.getElementsByClassName("tableRow");
+
+
+
+
+
+
+for(let i=0;i<removeFromCartBut.length;i++){
+    removeFromCartBut[i].onclick=()=>{
+        tableRow[i].style.display="none";
+        totalPrice -= parseInt(productsPriceTd[i].innerText);
+        console.log(totalPrice);
+    totalPriceContainer.innerHTML=`<h1> TOTAL TO PAY :${totalPrice}$</h1>`
+
+    }
+
+}
+
+
 
 
 function totalToPay(){
@@ -49,6 +72,3 @@ function totalToPay(){
 totalToPay();
 
 totalPriceContainer.innerHTML=`<h1> TOTAL TO PAY :${totalPrice}$</h1>`
-
-
-
--- JS/nike.js
+++ JS/nike.js
@@ -71,3 +71,5 @@ for (let i = 0; i < removeFromCart.length; i++) {
     }, 2000);
   };
 }
+
+console.log(PageCart)
